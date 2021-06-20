d6d8e1a7f08ca07ba7890e5108d89f07bb0a7d7e
Yagyu27 <arnovisdee@hotmail.com>
2021-06-20 11:43:23 -0500

    Ajustes de impresion

--- Bot2.0.py
+++ Bot2.0.py
@@ -178,7 +178,7 @@ def get_pokemons(text,update,context):
                 vacio3.append(valor)    
                 vacio3.append(".")
 
-        p = ' '.join(map(str,vacio3))
+        p = ''.join(map(str,vacio3))
         p = p.replace('.', '\n')
         update.message.reply_text("---Tipo---\n"+' - '.join(vacio1)+"\n---Habilidades---\n"+'  '.join(vacio2)+"\n---Stab Base---\n"+p)
         
